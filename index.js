fd35dd723808c785122e52621a58f18ce9360f8f
rebecca2000 <rebecca.w8886@gmail.com>
2021-12-31 12:18:47 +1100

    uibuild

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :c
 
 const cors = require('cors')
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
     { 
